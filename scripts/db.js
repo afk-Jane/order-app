d3a9b7d6117e4f7402554debffe7826df8898254
afk-Jane <j.domsgen@protonmail.com>
2025-03-25 14:42:35 +0100

    some functions and css added

---
+++ scripts/db.js
@@ -0,0 +1,47 @@
+let ourMenu = [{
+    'name' : 'DAS BUFFET',
+    'rating' : '4,2',
+    'description' : 'Frage nicht was ein Buffet, einfach DAS BUFFET!',
+    'deliveryPrice' : '3',
+    'maindishes' : [
+        {'name' : 'Döner (mit Fleisch)',
+        'description' : 'Fladenbrot mit Salat, Tomate, Gurke, Rotkraut, Weißkraut, Zwiebel, Könerfleisch vom Spieß und Kräuter-Jogurt-Soße',
+        'price' : '8'
+        },
+        {'name' : 'Döner (vegetarisch)',
+        'description' : 'Fladenbrot mit Salat, Tomate, Gurke, Rotkraut, Weißkraut, Zwiebel, verfeinert mit Käse und Kräuter-Jogurt-Soße',
+        'price' : '7'
+        },
+        {'name' : 'Halloumi Dürüm',
+            'description' : 'hauchdünnes Fladenbrot mit Salat, Tomate, Gurke, Rotkraut, Weißkraut, Zwiebel, Halloumi, verfeinert Kräuter-Jogurt-Soße',
+            'price' : '7,5'
+        },
+        {'name' : 'Pasta Napoli',
+            'description' : 'Tomatensoße mit Sellerie, Zwiebel und Knoblauch verfeinert mit Olivenöl und frischem Basilikum und Oregano',
+            'price' : '7'
+        },
+        {'name' : 'Falaffel-Bürger',
+            'description' : 'klassische Sesam-Burgerbuns mit einem saftigen Falaffel-Patty und Salat, Zwiebel, Tomate und Gurke und hausgemacht Burgersauce',
+            'price' : '8'
+        },
+    ],
+    'sidedishes' : [
+        {'name' : 'Pommes',
+            'description' : 'Pommes mit Ketschup und Mayo',
+            'price' : '3'
+        },
+        {'name' : 'Onion Rings',
+            'description' : 'Onion Rings mit Sauce nach Wahl',
+            'price' : '3,5'
+        },
+        {'name' : 'Cheese-Nuggets',
+            'description' : 'Cheese-Nuggets mit Sauce nach Wahl',
+            'price' : '3'
+        },
+        {'name' : 'Chili-Cheese-Nuggets',
+            'description' : 'Chili-Cheese-Nuggets mit Sauce nach Wahl',
+            'price' : '3'
+        },
+    ]
+    }
+]
