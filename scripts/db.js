bfb7a4fc3fb42e589cac797020aed4419da07f0e
afk-Jane <j.domsgen@protonmail.com>
2025-03-27 09:03:23 +0100

    many changes everywhere

--- scripts/db.js
+++ scripts/db.js
@@ -6,24 +6,24 @@ let restaurant = [
         'deliveryPrice' : '3',
         'maindishes' : [
             {'name' : 'Döner (mit Fleisch)',
-            'description' : 'Fladenbrot mit Salat, Tomate, Gurke, Rotkraut, Weißkraut, Zwiebel, Könerfleisch vom Spieß und Kräuter-Jogurt-Soße',
-            'price' : '8'
+            'description' : 'Fladenbrot mit Salat, Tomate, Gurke, Rotkraut, Weißkraut, Zwiebel, Dönerfleisch vom Spieß und Kräuter-Jogurt-Soße',
+            'price' : 8
             },
             {'name' : 'Döner (vegetarisch)',
             'description' : 'Fladenbrot mit Salat, Tomate, Gurke, Rotkraut, Weißkraut, Zwiebel, verfeinert mit Käse und Kräuter-Jogurt-Soße',
-            'price' : '7'
+            'price' : 7
             },
             {'name' : 'Halloumi Dürüm',
                 'description' : 'hauchdünnes Fladenbrot mit Salat, Tomate, Gurke, Rotkraut, Weißkraut, Zwiebel, Halloumi, verfeinert Kräuter-Jogurt-Soße',
-                'price' : '7,5'
+                'price' : 7.5
             },
             {'name' : 'Pasta Napoli',
                 'description' : 'Tomatensoße mit Sellerie, Zwiebel und Knoblauch verfeinert mit Olivenöl und frischem Basilikum und Oregano',
-                'price' : '7'
+                'price' : 7
             },
             {'name' : 'Falaffel-Bürger',
                 'description' : 'klassische Sesam-Burgerbuns mit einem saftigen Falaffel-Patty und Salat, Zwiebel, Tomate und Gurke und hausgemacht Burgersauce',
-                'price' : '8'
+                'price' : 8
             },
         ],
         'sidedishes' : [
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,13 @@
 function getDishTemplate(dish, indexDish) {
     let formattedPrice = parseFloat(dish.price).toFixed(2);
-    
     return `
         <div class="a-dish-template" id="dish-${indexDish}">
-            <h3 id="dish-name-${indexDish}">${dish.name}</h3>
-            <button class="add-btn" id="add-btn-${indexDish}" onclick="addToCart(${indexDish})">+</button>
+            <div class='dish-first-row'>
+                <h3 id="dish-name-${indexDish}">${dish.name}</h3>
+                <button class="add-btn" id="add-btn-${indexDish}" onclick="addToCart(${indexDish})">+</button>
+            </div>    
             <p class="dish-description" id="dish-description-${indexDish}">${dish.description}</p>
-            <p class="dish-price" id="dish-price-${indexDish}">${dish.price}€</p>
+            <p class="dish-price" id="dish-price-${indexDish}">${formattedPrice}€</p>
         </div>
     `;
 }
